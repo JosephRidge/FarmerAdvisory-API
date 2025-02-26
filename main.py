c3908bb4d6304399e08e6370c2d2529b14fd09f6
JosephRidge <josephridge18@gmail.com>
2025-02-26 13:15:57 +0300

    The Genesis

---
+++ main.py
@@ -0,0 +1,29 @@
+from typing import Union
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI(
+    title="Farmer Advisor - API",
+    version="1.0",)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "query": q}
+
+
+# type __annotations is import
---
+++ readMe.md
@@ -0,0 +1,5 @@
+# Architecture:
+
+![alt text](image.png)
+
+---
